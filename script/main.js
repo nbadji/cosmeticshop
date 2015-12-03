c55a49880da6218758358156eb0c7e345f4c9b8b
agarand <alexandre_307@hotmail.com>
2015-12-03 13:12:45 -0500

    v1 alexandre

--- script/main.js
+++ script/main.js
@@ -2,5 +2,12 @@
  * Created by nbadji on 2015-11-27.
  */
 "use strict";
-var cosmetic ;
-// fdfdfsfsdf
+function colorer_decolorer() {
+    this.classList.toggle("survole"); // Inverse la classe survole
+}
+var les_listes = document.querySelectorAll("li");
+
+for (var i = 0; i <= les_listes.length; i++) {
+    les_listes[i].addEventListener("mouseover" , colorer_decolorer);
+    les_listes[i].addEventListener("mouseout" , colorer_decolorer);
+}
